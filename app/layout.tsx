66486ef336abb2c2b72096f5d2e26a36eccb2d9d
SerenaShar <33sereshar@csdecatur.net>
2025-01-15 16:19:37 -0500

    Fixed error

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
