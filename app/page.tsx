f4206a6ea33f72ca3abedc38df2508fedfd7ac79
SerenaShar <33sereshar@csdecatur.net>
2025-02-05 16:11:59 -0500

    Fixed font size

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-     <h1 className='text-[200]'>TECH CLUB</h1>
+     <h1 className='text-[200px]'>TECH CLUB</h1>
     </div>
   );
 }
