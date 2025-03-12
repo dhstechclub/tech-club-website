9d60c00b308ca41b6100a713b12cffb171a5f6dc
SerenaShar <33sereshar@csdecatur.net>
2025-03-12 16:26:53 -0400

    Added logo

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,10 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-     <h1 className='text-[100px]'>TECH CLUB</h1><hr></hr><br></br>
+     <h1 className='text-[100px]'>TECH CLUB</h1>
+     <div className='flex justify-center'>
+     <img src='/TechClubLogo.png' width='75px'></img></div><br></br>
+     <hr></hr><br></br>
     <h2>About:</h2>
     <p>Tech club is a club where people learn how to code and other stuff. </p>
 
